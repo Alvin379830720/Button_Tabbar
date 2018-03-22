35791c4b7d5882702309d0519a1315be473a939e
斯远 <sifar@iMac.local>
2018-03-22 10:55:27 +0800

    仿照tabbar的uibutton，可完全自适应图片与文字的size。

---
+++ Button_Tabbar/Button_Tabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Button_Tabbar
+//
+//  Created by 斯远 on 22/03/2018.
+//  Copyright © 2018 WuYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
